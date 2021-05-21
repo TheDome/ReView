d9eb2ee26ebebc9365316d1ba5bc2f262287b4a4
TheDome <dominic.heun@gmx.net>
2021-05-21 18:45:18 +0200

    Fix auth URL

--- src/remarkable/constants.rs
+++ src/remarkable/constants.rs
@@ -17,7 +17,7 @@ const AUTH0_USER: &str = "auth0|5ff43c03c9f7b3013eeec9a7";
 
 pub const PROTOCOL: &str = "https://";
 
-pub const REMARKABLE_SESSION_BASE_API: &str = "my.remarkable.com";
+pub const REMARKABLE_SESSION_BASE_API: &str = "webapp-production-dot-remarkable-production.appspot.com";
 pub const REMARKABLE_SESSION_TOKEN_NEW: &str = "/token/json/2/user/new";
 pub const REMARKABLE_SESSION_TOKEN_NEW_DEVICE: &str = "/token/json/2/device/new";
 
